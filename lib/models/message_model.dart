7135861a6788141d6e74ef9497d4a50fdf570a2a
Faraz Hussain <fraz.hussain12@gmail.com>
2024-03-21 02:53:50 -0800

    fahad

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -4,13 +4,15 @@ class MessageModel {
   String? text;
   bool? seen;
   DateTime? createdOn;
+  String? imageUrl; // Add imageUrl field for storing image URL
 
   MessageModel({
     this.sender,
     this.text,
     this.seen,
     this.createdOn,
-    this.messageId
+    this.messageId,
+    this.imageUrl, // Update constructor to accept imageUrl
   });
 
   factory MessageModel.fromJson(Map<String, dynamic> json) {
@@ -18,8 +20,9 @@ class MessageModel {
       sender: json['sender'],
       text: json['text'],
       seen: json['seen'],
-      createdOn: json['createdOn'].toDate()  ,
-      messageId:json['messageId']
+      createdOn: json['createdOn'].toDate(),
+      messageId: json['messageId'],
+      imageUrl: json['imageUrl'], // Parse imageUrl from JSON
     );
   }
 
@@ -29,7 +32,45 @@ class MessageModel {
       'text': text,
       'seen': seen,
       'createdOn': createdOn,
-      'messageId':messageId
+      'messageId': messageId,
+      'imageUrl': imageUrl, // Include imageUrl in JSON serialization
     };
   }
 }
+
+
+// class MessageModel {
+//   String? messageId;
+//   String? sender;
+//   String? text;
+//   bool? seen;
+//   DateTime? createdOn;
+
+//   MessageModel({
+//     this.sender,
+//     this.text,
+//     this.seen,
+//     this.createdOn,
+//     this.messageId
+//   });
+
+//   factory MessageModel.fromJson(Map<String, dynamic> json) {
+//     return MessageModel(
+//       sender: json['sender'],
+//       text: json['text'],
+//       seen: json['seen'],
+//       createdOn: json['createdOn'].toDate()  ,
+//       messageId:json['messageId']
+//     );
+//   }
+
+//   Map<String, dynamic> toJson() {
+//     return {
+//       'sender': sender,
+//       'text': text,
+//       'seen': seen,
+//       'createdOn': createdOn,
+//       'messageId':messageId
+//     };
+//   }
+// }
--- lib/views/chat_room_screen.dart
+++ lib/views/chat_room_screen.dart
@@ -1,11 +1,16 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:chatapp/main.dart';
 import 'package:chatapp/models/chat_room_model.dart';
 import 'package:chatapp/models/message_model.dart';
 import 'package:chatapp/models/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
 class ChatRoomPage extends StatefulWidget {
   final UserModel? targetUser;
@@ -19,29 +24,90 @@ class ChatRoomPage extends StatefulWidget {
 }
 
 class _ChatRoomPageState extends State<ChatRoomPage> {
-TextEditingController messageController = TextEditingController();
+  TextEditingController messageController = TextEditingController();
 
-  void sendMessage() async {
+  // void sendMessage() async {
+  //   String msg = messageController.text.trim();
+  //   messageController.clear();
+
+  //   if (msg != "") {
+  //     // Send Message
+  //     MessageModel newMessage = MessageModel(
+  //         messageId: uuid.v1(),
+  //         sender: widget.userModel!.uid,
+  //         createdOn: DateTime.now(),
+  //         text: msg,
+  //         seen: false);
+
+  //     FirebaseFirestore.instance
+  //         .collection("chatrooms")
+  //         .doc(widget.chatRoomModel!.roomId)
+  //         .collection("messages")
+  //         .doc(newMessage.messageId)
+  //         .set(newMessage.toJson());
+
+  //     widget.chatRoomModel!.lastMessage = msg;
+  //     FirebaseFirestore.instance
+  //         .collection("chatrooms")
+  //         .doc(widget.chatRoomModel!.roomId)
+  //         .set(widget.chatRoomModel!.toJson());
+
+  //     log("Message Sent!");
+  //   }
+  // }
+
+  void sendMessage([String? imageUrl]) async {
     String msg = messageController.text.trim();
     messageController.clear();
 
-    if(msg != "") {
+    if (msg != "" || imageUrl != null) {
       // Send Message
       MessageModel newMessage = MessageModel(
         messageId: uuid.v1(),
         sender: widget.userModel!.uid,
         createdOn: DateTime.now(),
         text: msg,
-        seen: false
+        seen: false,
+        imageUrl: imageUrl, // Add the imageUrl to the message model
       );
 
-      FirebaseFirestore.instance.collection("chatrooms").doc(widget.chatRoomModel!.roomId).collection("messages").doc(newMessage.messageId).set(newMessage.toJson());
+      FirebaseFirestore.instance
+          .collection("chatrooms")
+          .doc(widget.chatRoomModel!.roomId)
+          .collection("messages")
+          .doc(newMessage.messageId)
+          .set(newMessage.toJson());
 
       widget.chatRoomModel!.lastMessage = msg;
-      FirebaseFirestore.instance.collection("chatrooms").doc(widget.chatRoomModel!.roomId).set(widget.chatRoomModel!.toJson());
+      FirebaseFirestore.instance
+          .collection("chatrooms")
+          .doc(widget.chatRoomModel!.roomId)
+          .set(widget.chatRoomModel!.toJson());
 
       log("Message Sent!");
     }
+  }
+
+  void pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      uploadImage(File(pickedImage.path));
+    }
+  }
+
+  void uploadImage(File imageFile) async {
+    var reference =
+        FirebaseStorage.instance.ref().child('chat_images/${Uuid().v1()}');
+
+    var uploadTask = reference.putFile(imageFile);
+
+    var imageUrl = await uploadTask.then((taskSnapshot) {
+      return taskSnapshot.ref.getDownloadURL();
+    });
+
+    sendMessage(imageUrl);
   }
 
   @override
@@ -50,82 +116,112 @@ TextEditingController messageController = TextEditingController();
       appBar: AppBar(
         title: Row(
           children: [
-
             CircleAvatar(
               backgroundColor: Colors.grey[300],
-              backgroundImage: NetworkImage(widget.targetUser!.profileImage.toString()),
+              backgroundImage:
+                  NetworkImage(widget.targetUser!.profileImage.toString()),
             ),
-
-        const    SizedBox(width: 10,),
-
+            const SizedBox(
+              width: 10,
+            ),
             Text(widget.targetUser!.fullName.toString()),
-
           ],
         ),
       ),
       body: SafeArea(
-        child:  Container(
+        child: Container(
           child: Column(
             children: [
-
               // This is where the chats will go
               Expanded(
                 child: Container(
-                  padding:const EdgeInsets.symmetric(
-                    horizontal: 10
-                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
                   child: StreamBuilder(
-                    stream: FirebaseFirestore.instance.collection("chatrooms").doc(widget.chatRoomModel!.roomId).collection("messages").orderBy("createdOn", descending: true).snapshots(),
+                    stream: FirebaseFirestore.instance
+                        .collection("chatrooms")
+                        .doc(widget.chatRoomModel!.roomId)
+                        .collection("messages")
+                        .orderBy("createdOn", descending: true)
+                        .snapshots(),
                     builder: (context, snapshot) {
-                      if(snapshot.connectionState == ConnectionState.active) {
-                        if(snapshot.hasData) {
-                          QuerySnapshot dataSnapshot = snapshot.data as QuerySnapshot;
+                      if (snapshot.connectionState == ConnectionState.active) {
+                        if (snapshot.hasData) {
+                          QuerySnapshot dataSnapshot =
+                              snapshot.data as QuerySnapshot;
 
                           return ListView.builder(
                             reverse: true,
                             itemCount: dataSnapshot.docs.length,
                             itemBuilder: (context, index) {
-                              MessageModel currentMessage = MessageModel.fromJson(dataSnapshot.docs[index].data() as Map<String, dynamic>);
+                              MessageModel currentMessage =
+                                  MessageModel.fromJson(dataSnapshot.docs[index]
+                                      .data() as Map<String, dynamic>);
 
                               return Row(
-                                mainAxisAlignment: (currentMessage.sender == widget.userModel!.uid) ? MainAxisAlignment.end : MainAxisAlignment.start,
+                                mainAxisAlignment: (currentMessage.sender ==
+                                        widget.userModel!.uid)
+                                    ? MainAxisAlignment.end
+                                    : MainAxisAlignment.start,
                                 children: [
-                                  Container(
-                                    margin: const EdgeInsets.symmetric(
-                                      vertical: 2,
-                                    ),
-                                    padding:const EdgeInsets.symmetric(
-                                      vertical: 10,
-                                      horizontal: 10,
-                                    ),
-                                    decoration: BoxDecoration(
-                                      color: (currentMessage.sender == widget.userModel!.uid) ? Colors.grey : Theme.of(context).colorScheme.secondary,
-                                      borderRadius: BorderRadius.circular(5),
-                                    ),
-                                    child: Text(
-                                      currentMessage.text.toString(),
-                                      style:const TextStyle(
-                                        color: Colors.white,
+                                  if (currentMessage.imageUrl !=
+                                      null) // Check if message contains an image URL
+                                    Container(
+                                      margin: const EdgeInsets.symmetric(
+                                        vertical: 2,
                                       ),
-                                    )
-                                  ),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: Image.network(
+                                        currentMessage.imageUrl!,
+                                        width:
+                                            150, // Adjust the width according to your needs
+                                        height:
+                                            150, // Adjust the height according to your needs
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  if (currentMessage.text !=
+                                      null) // Check if message contains text
+                                    Container(
+                                      margin: const EdgeInsets.symmetric(
+                                        vertical: 2,
+                                      ),
+                                      padding: const EdgeInsets.symmetric(
+                                        vertical: 10,
+                                        horizontal: 10,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: (currentMessage.sender ==
+                                                widget.userModel!.uid)
+                                            ? Colors.grey
+                                            : Theme.of(context)
+                                                .colorScheme
+                                                .secondary,
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: Text(
+                                        currentMessage.text!,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
                                 ],
                               );
                             },
                           );
-                        }
-                        else if(snapshot.hasError) {
+                        } else if (snapshot.hasError) {
                           return const Center(
-                            child: Text("An error occured! Please check your internet connection."),
+                            child: Text(
+                                "An error occured! Please check your internet connection."),
                           );
-                        }
-                        else {
+                        } else {
                           return const Center(
                             child: Text("Say hi to your new friend"),
                           );
                         }
-                      }
-                      else {
+                      } else {
                         return const Center(
                           child: CircularProgressIndicator(),
                         );
@@ -137,35 +233,37 @@ TextEditingController messageController = TextEditingController();
 
               Container(
                 color: Colors.grey[200],
-                padding:const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 5
-                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
                 child: Row(
                   children: [
-
                     Flexible(
                       child: TextField(
                         controller: messageController,
                         maxLines: null,
-                        decoration:const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Enter message"
-                        ),
+                        decoration: InputDecoration(
+                            suffixIcon: GestureDetector(
+                                onTap: () {},
+                                child: const Icon(
+                                  Icons.image,
+                                  color: Colors.blue,
+                                )),
+                            border: InputBorder.none,
+                            hintText: "Enter message"),
                       ),
                     ),
-
                     IconButton(
                       onPressed: () {
                         sendMessage();
                       },
-                      icon: Icon(Icons.send, color: Theme.of(context).colorScheme.secondary,),
+                      icon: Icon(
+                        Icons.send,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
                     ),
-
                   ],
                 ),
               ),
-
             ],
           ),
         ),
