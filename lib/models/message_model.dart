78f024b803601f797f1855a55cbf9e4c910366b2
Faraz Hussain <fraz.hussain12@gmail.com>
2024-03-21 03:13:33 -0800

    fahad

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -22,7 +22,7 @@ class MessageModel {
       seen: json['seen'],
       createdOn: json['createdOn'].toDate(),
       messageId: json['messageId'],
-      imageUrl: json['imageUrl'], // Parse imageUrl from JSON
+      imageUrl: json['imageUrl'],  
     );
   }
 
@@ -33,44 +33,9 @@ class MessageModel {
       'seen': seen,
       'createdOn': createdOn,
       'messageId': messageId,
-      'imageUrl': imageUrl, // Include imageUrl in JSON serialization
+      'imageUrl': imageUrl,  
     };
   }
 }
 
-
-// class MessageModel {
-//   String? messageId;
-//   String? sender;
-//   String? text;
-//   bool? seen;
-//   DateTime? createdOn;
-
-//   MessageModel({
-//     this.sender,
-//     this.text,
-//     this.seen,
-//     this.createdOn,
-//     this.messageId
-//   });
-
-//   factory MessageModel.fromJson(Map<String, dynamic> json) {
-//     return MessageModel(
-//       sender: json['sender'],
-//       text: json['text'],
-//       seen: json['seen'],
-//       createdOn: json['createdOn'].toDate()  ,
-//       messageId:json['messageId']
-//     );
-//   }
-
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'sender': sender,
-//       'text': text,
-//       'seen': seen,
-//       'createdOn': createdOn,
-//       'messageId':messageId
-//     };
-//   }
-// }
+ 
